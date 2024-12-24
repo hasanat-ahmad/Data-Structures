f26f99a7676c4e619f926e12d8fbd3402c4f6d5d
Sahibzada Hasanat Ahmad <152200836+hasanat-ahmad@users.noreply.github.com>
2024-12-24 14:57:53 +0500

    Add files via upload

--- Sorting Algorithms/BinarySearchThroughIteration.cpp
+++ Sorting Algorithms/BinarySearchThroughIteration.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void binarySearch(int arr[], int size, int start, int end, int target)
+void binarySearch(int arr[], int start, int end, int target)
 {
-    while (start <= end){
-        int mid = (start + end)/2;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
 
-        if (target < arr[mid]){
+        if (target < arr[mid])
+        {
             end = mid - 1;
         }
-        else if (target > arr[mid]){
+        else if (target > arr[mid])
+        {
             start = mid + 1;
         }
-        else{
-            cout << "Target found";
+        else
+        {
+            cout << "Target found at index: " << mid;
             return;
         }
     }
@@ -24,9 +28,7 @@ int main()
 
     int arr[] = {1, 2, 4, 6, 7, 9};
     int start = 0;
-    int size = sizeof(arr)/sizeof(arr[0]);
-    int end = sizeof(arr)/sizeof(arr[0]) - 1;
-
-    binarySearch(arr, size, start , end, 10);
+    int end = sizeof(arr) / sizeof(arr[0]) - 1;
+    binarySearch(arr, start, end, 9);
     return 0;
 }
